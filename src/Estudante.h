89f28692c52fcb5bf4ac462679cabbc81935da32
Anete <anetempereira@gmail.com>
2022-10-22 11:40:33 +0100

    added a new classes

--- src/Estudante.h
+++ src/Estudante.h
@@ -5,8 +5,26 @@
 #ifndef SRC_ESTUDANTE_H
 #define SRC_ESTUDANTE_H
 
+#include <string>
+#include <iostream>
+
+using namespace std;
 
 class Estudante {
+private:
+
+    /**
+     * \brief Número do estudante.
+     */
+    int studentCode;
+
+    /**
+     * \brief Nome do estudante.
+     */
+    string studentName;
+
+public:
+
 
 };
 
---
+++ src/LogisticaDeTurmas.h
@@ -0,0 +1,14 @@
+//
+// Created by anete on 22/10/2022.
+//
+
+#ifndef SRC_LOGISTICADETURMAS_H
+#define SRC_LOGISTICADETURMAS_H
+
+
+class LogisticaDeTurmas {
+
+};
+
+
+#endif //SRC_LOGISTICADETURMAS_H
