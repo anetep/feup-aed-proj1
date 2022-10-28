4a329e8678eba2bb0a123acab438b01768eccd88
Gabriel <up202108816@up.pt>
2022-10-28 15:00:32 +0100

    Created BST.h and BST.cpp

---
+++ src/BinarySearchTree.h
@@ -0,0 +1,17 @@
+//
+// Created by worker on 28/10/2022.
+//
+
+#ifndef SRC_BINARYSEARCHTREE_H
+#define SRC_BINARYSEARCHTREE_H
+
+#include <iostream>
+
+using namespace std;
+
+
+class BST{
+
+};
+
+#endif //SRC_BINARYSEARCHTREE_H
