7f594a62f96b7d18d7e50c8bb81973ec42ec50a2
Anete <anetempereira@gmail.com>
2022-11-02 00:06:30 +0000

    added new version of students class with new functions and accessing file

--- src/Estudante.cpp
+++ src/Estudante.cpp
@@ -1,10 +1,61 @@
 //
-// Created by anete on 20/10/2022.
+// Created by anete on 01/11/2022.
+//
 
 #include "Estudante.h"
+using namespace std;
 
 int Estudante::getStudentCode() {return studentCode;}
 string Estudante::getStudentName() {return studentName;}
 
 void Estudante::setStudentName(string name) {this->studentName = name;}
 void Estudante::setStudentCode(int code) {this->studentCode = code;}
+
+void Estudante::addNewStudent(int &code, string &name) {
+    pair <int, string> student = make_pair(code, name);
+    allStudents.push_back(student);
+}
+
+void Estudante::addVectorOfStudents(vector<pair<int, string>> &lista){
+    removeDuplicates(lista);
+    for (const auto& pair : lista){
+        allStudents.push_back(pair);
+    }
+}
+
+
+void Estudante::printAllStudents(){
+    for (const auto& pair : allStudents){
+        cout << "Nome: " << pair.second << " -- Nr: " << pair.first << endl;
+    }
+}
+
+vector<pair<int, string>> Estudante::getAllStudents(){
+    return allStudents;
+}
+
+
+vector<pair<int, string>> Estudante::sortStudentsByNum(){
+    sort(allStudents.begin(), allStudents.end());
+    return allStudents;
+}
+
+//Only an auxiliar function to help sort the students by name
+bool compareStudentsNames(pair<int, string> &x, pair<int, string> &y) {
+    return x.second < y.second;
+}
+
+vector<pair<int, string>> Estudante::sortStudentsByName(){
+    sort(allStudents.begin(), allStudents.end(), compareStudentsNames);
+    return allStudents;
+}
+
+vector<pair<int, string>> Estudante::removeDuplicates(vector<pair<int, string>> &st){
+    set<pair<int, string>> unique;
+    for (const auto& pair : st){unique.insert(pair);}
+    st.clear();
+    for (const auto& pair : unique){st.push_back(pair);}
+    return st;
+}
+
+
--- src/Estudante.h
+++ src/Estudante.h
@@ -1,12 +1,17 @@
 //
-// Created by anete on 20/10/2022.
+// Created by anete on 01/11/2022.
 //
+
 
 #ifndef SRC_ESTUDANTE_H
 #define SRC_ESTUDANTE_H
 
 #include <string>
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+
 
 using namespace std;
 
@@ -15,19 +20,35 @@ class Estudante {
 private:
     int studentCode;
     string studentName;
+    vector <pair<int, string>> allStudents;
 
 public:
 
-    Estudante();
+    //Estudante();
 
-    Estudante(int studentCode, string studentName);
+    //Estudante(int studentCode, string studentName);
 
     int getStudentCode();
     string getStudentName();
 
     void setStudentName(string name);
-    void setStudentCode(int code);
-};
 
+    void setStudentCode(int code);
+
+    void addNewStudent(int &code, string &name);
+
+    void addVectorOfStudents(vector<pair<int, string>> &lista);
+
+    vector<pair<int, string>> getAllStudents();
+
+    void printAllStudents();
+
+    vector<pair<int, string>> sortStudentsByNum();
+
+    vector<pair<int, string>> sortStudentsByName();
+
+    vector<pair<int, string>> removeDuplicates(vector<pair<int, string>> &st);
+
+};
 
 #endif //SRC_ESTUDANTE_H
