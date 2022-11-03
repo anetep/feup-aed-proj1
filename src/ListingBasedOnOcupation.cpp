bb38a3239c629c151dd92d9f5e33bee9ac27f6bf
Anete <anetempereira@gmail.com>
2022-11-03 23:38:17 +0000

    added new class to have functions to verify and print the listing of different things

--- src/ListingBasedOnOcupation.cpp
+++ src/ListingBasedOnOcupation.cpp
@@ -3,3 +3,44 @@
 //
 
 #include "ListingBasedOnOcupation.h"
+
+vector <pair<string, string>> ListingBasedOnOcupation::CompareClassOcupation(vector<Estudante> &estudantes, string classD){
+    vector<pair<string, string>> studOcup;
+    for (const auto &st: estudantes){
+        for (const auto &turm: st.getStudentSchedule()){
+            if (turm.getClassCode() == classD){
+                pair<string, string> aux = {st.getStudentName(), turm.getUcCode()};
+                studOcup.push_back(aux);
+            }
+        }
+    }
+    return studOcup;
+}
+
+void ListingBasedOnOcupation::PrintClassOcupation(){
+
+    ReadFiles o;
+    vector<Estudante> s = o.readStudentsFile();
+    vector<Estudante> cleanS = o.joinStudentClases(s);
+
+    cout << "turma: 3LEIC08" << endl; // anete e carminho
+    vector<pair<string, string>> res = CompareClassOcupation(cleanS, "3LEIC08");
+    for (auto r: res){
+        cout << r.first << " na unidade curricular " << r.second << endl;
+    }
+    cout << "===================" << endl;
+
+    cout << "turma: 1LEIC08" << endl; // gisela e carminho
+    vector<pair<string, string>> res2 = CompareClassOcupation(cleanS, "1LEIC08");
+    for (auto r: res2){
+        cout << r.first << " na unidade curricular " << r.second << endl;
+    }
+    cout << "===================" << endl;
+
+    cout << "turma: 1LEIC05" << endl; // iara
+    vector<pair<string, string>> res3 = CompareClassOcupation(cleanS, "1LEIC05");
+    for (auto r: res3){
+        cout << r.first << " na unidade curricular " << r.second << endl;
+    }
+    cout << "===================" << endl;
+}
--- src/ListingBasedOnOcupation.h
+++ src/ListingBasedOnOcupation.h
@@ -5,8 +5,16 @@
 #ifndef SRC_LISTINGBASEDONOCUPATION_H
 #define SRC_LISTINGBASEDONOCUPATION_H
 
+#include "Estudante.h"
+#include "ReadFiles.h"
+#include <list>
+
+
 
 class ListingBasedOnOcupation {
+public:
+    vector <pair<string, string>> CompareClassOcupation(vector<Estudante> &estudantes, string classD);
+    void PrintClassOcupation();
 
 };
 
