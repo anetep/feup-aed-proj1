73dbf9d5a9660211521c1e749091a70943e020f6
Anete <anetempereira@gmail.com>
2022-11-03 23:10:17 +0000

    tested in main function to join students classes comming from file

---
+++ src/ListingBasedOnOcupation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by anete on 03/11/2022.
+//
+
+#include "ListingBasedOnOcupation.h"
---
+++ src/ListingBasedOnOcupation.h
@@ -0,0 +1,14 @@
+//
+// Created by anete on 03/11/2022.
+//
+
+#ifndef SRC_LISTINGBASEDONOCUPATION_H
+#define SRC_LISTINGBASEDONOCUPATION_H
+
+
+class ListingBasedOnOcupation {
+
+};
+
+
+#endif //SRC_LISTINGBASEDONOCUPATION_H
